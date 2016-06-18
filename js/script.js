af9b1cb5cb7af8f8e43ac7fab9dd9e8100db9393
Gracie Lundell <gracielundell@gmail.com>
2016-06-18 16:51:33 -0700

    draw cells depending on if they are true or false

--- js/script.js
+++ js/script.js
@@ -3,7 +3,7 @@
 // live cell more than 3 neighbors dies
 // dead cell w 3 neighbors becomes alive
 
-var draw = function(x, y) {
+var drawCell = function(x, y) {
   var c=document.getElementById("canvas");
   var ctx=c.getContext("2d");
   ctx.lineWidth="1";
@@ -12,21 +12,43 @@ var draw = function(x, y) {
   ctx.stroke();
 };
 
+// sets all values in array to false
 var initialize = function(gridWidth, gridHeight) {
   var grid = [];
-  for (i=0; i<=gridWidth; i++) {
+  for (i=0; i<gridWidth; i++) {
     // push arrays in
     grid.push([]);
-    for (j=0; j<=gridHeight; j++) {
+    for (j=0; j<gridHeight; j++) {
       // push booleans into pushed in arrays
       grid[i].push(false);
     }
   }
-  console.log(grid);
+  return grid;
 };
 
-// var grid = initialize();
+var grid = initialize(300, 300);
+console.log(grid);
 
-initialize(3, 3);
+// function that loops through the whole nested array
+// if false, draw cell
+// if true, do nothing
+var draw = function() {
+  for (i=0; i< grid.length; i++) {
+    for (j=0; j< grid.length; j++) {
+      if (grid[i][j] === true) {
+        drawCell(i, j);
+      }
+    }
+  }
+  console.log(grid[2][2]);
+};
 
-draw(20, 20);
+
+grid[2][2] = true;
+grid[100][100] = true;
+grid[230][230] = true;
+grid[2][60] = true;
+grid[100][250] = true;
+grid[230][100] = true;
+
+draw();
